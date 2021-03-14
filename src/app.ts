a6f443571e03be4c5e2083efac7afc8eea1eb353
pezzu <7243796+pezzu@users.noreply.github.com>
2021-03-14 18:04:26 -0400

    Helmet added

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import helmet from "helmet";
 import ApiRouter from "./routes";
 
 const app = express();
 
+app.use(helmet());
 app.use(express.json());
 app.use("/api", ApiRouter.routes());
 
