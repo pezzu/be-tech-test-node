8339a39640231fc2e3ff6e806427a47843af90d2
pezzu <7243796+pezzu@users.noreply.github.com>
2021-03-15 09:08:57 -0400

    Auth tests fixed

--- src/app/auth/auth.spec.ts
+++ src/app/auth/auth.spec.ts
@@ -1,5 +1,57 @@
 import request from "supertest";
+import mongoose from "mongoose";
 import app from "../../app";
+import { User } from "../auth/model/user";
+
+const db = {
+  users: [
+    {
+      name: "admin",
+      password: "admin",
+      role: "Admin",
+    },
+    {
+      name: "editor",
+      password: "verysecretpassword",
+      role: "Editor",
+    },
+    {
+      name: "tester",
+      password: "123",
+      role: "Tester",
+    },
+  ]
+}
+
+beforeAll(async () => {
+  await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
+
+beforeEach(async (done) => {
+  try {
+    await User.insertMany(db.users);
+    done();
+  } catch (err) {
+    done(err);
+  }
+});
+
+afterEach(async (done) => {
+  try {
+    await User.remove({}).exec();
+    done();
+  } catch (err) {
+    done(err);
+  }
+});
 
 describe("POST /api/auth", () => {
   it("Returns token for authenticated user", (done) => {
